acc5ded6150987400242734bc48b6f8d2fd2b62b
riskalaluna <riskalaluna@gmail.com>
2023-06-16 11:00:46 +0700

    bool empty

--- StackArray_176/StackArray_176.cpp
+++ StackArray_176/StackArray_176.cpp
@@ -37,4 +37,9 @@ public:
 		cout << "\nThe popped element is:" << stack_array[top] << endl; //step 2
 		top--; //step 3 decrement
 	}
+
+	//method for check if data is empty
+	bool empty() {
+		return (top == -1);
+	}
 };
