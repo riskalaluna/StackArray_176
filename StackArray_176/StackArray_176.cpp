b980f0f41950efb3b32a4baf8407d345089cae95
riskalaluna <riskalaluna@gmail.com>
2023-06-16 10:59:18 +0700

    9

--- StackArray_176/StackArray_176.cpp
+++ StackArray_176/StackArray_176.cpp
@@ -33,5 +33,8 @@ public:
 			cout << "\nStack is empty. Cannot pop." << endl; //1.a
 			return; //1.b
 		}
+
+		cout << "\nThe popped element is:" << stack_array[top] << endl; //step 2
+		top--; //step 3 decrement
 	}
 };
